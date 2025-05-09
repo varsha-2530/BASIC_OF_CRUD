6e2b5aa4f13b576c64871f7bf7b339133025de4e
ADMIN <admin@ADMINs-MacBook-Pro.local>
2025-05-09 15:02:14 +0530

    addUser Data

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require('express');
+const members = require("./Members");
+const uuid = require('uuid')
 const app = express();
+
+
 const PORT = 3000;
 
-const members = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john231@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Steve',
-        email: 'stv123@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 3,
-        name: 'Steve John',
-        email: 'sj98@gmail.com',
-        status: 'inactive'
-    }
-];
+app.use(express.json())
+
+
 
 app.get('/showAllUser', (req, res) => {
     res.status(200).json(members);
@@ -36,10 +23,26 @@ app.get("/showUser/:userId", (req, res) => {
 
     let user = members.filter(member => member.id === id);
     //console.log('get the user');
-    
+
 
     (user.length !== 0) ? res.status(200).json(user) : res.status(404).json({ message: `User not found with id ${id}` })
 });
+
+app.post('/addUser', (request, response) => {
+    console.log('User :', request.body)
+    // const name = request.body.name ;
+    // const email = request.body.email ;
+    const {name,email} =  request.body
+    console.log(name,email);
+
+    members.push({
+        id : uuid.v4(),
+        name,
+        email
+    })
+
+    response.status(200).json(members)
+})
 
 
 
